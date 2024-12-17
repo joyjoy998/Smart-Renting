640845129285937327f963ac212459b1b0fac1db
Haochuan Zhou <mrzhouhc@gmail.com>
2024-12-17 14:12:54 +1100

    Try to use Google Map API to generate map

---
+++ src/lib/maps/constants.ts
@@ -0,0 +1,16 @@
+export const MAPS_CONFIG = {
+  defaultCenter: {
+    lat: 35.6762,
+    lng: 139.6503,
+  },
+  defaultZoom: 14,
+  mapOptions: {
+    panControl: true,
+    zoomControl: true,
+    mapTypeControl: true,
+    scaleControl: true,
+    streetViewControl: true,
+    rotateControl: true,
+    fullscreenControl: true,
+  },
+};
