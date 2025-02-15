200e57620945913bb50e75cef49610e24aaa98c4
Yara <yarawang999@gmail.com>
2025-02-15 19:05:50 +1100

    Revert "Revert "feat: add search combobox for Google Maps""
    This reverts commit 774b40d12555477fbfc8243f4c1a99de799bf046.

--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -2,13 +2,15 @@ import { ModeToggle } from "@/components/theme/ModeToggle";
 import { AccountButton } from "./AccountButton";
 import { LanguageToggle } from "../userCustomization/LanguageToggle";
 import { SearchBox } from "./SearchBox";
+import { useMap } from "@vis.gl/react-google-maps";
+import { useEffect } from "react";
 
-export function Header() {
+export function Header() {  
   return (
     <header className="fixed top-0 w-full z-50 ">
       <div className="h-full flex items-center justify-between gap-4">
         <div className="flex items-center gap-4">
-          <SearchBox />
+          {/* <SearchBox /> */}
         </div>
 
         <div className="flex items-center gap-2">
