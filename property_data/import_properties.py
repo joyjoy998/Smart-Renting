afbb4475a289b32b92949f3c4c0637601c452adb
quilla3 <141195630+quilla3@users.noreply.github.com>
2025-02-22 23:01:51 +1100

    - Deleted 1 invalid property data from properties json files. - Successfully imported Wollongong properties data into database. - Relocated and modified `import_properties.py` to keep the project folder clean.

--- property_data/import_properties.py
+++ property_data/import_properties.py
@@ -2,13 +2,13 @@ from dotenv import load_dotenv
 import os
 import json
 import psycopg2
-load_dotenv(".env.local") 
+load_dotenv("../.env.local") 
 
 DB_HOST = os.environ.get("POSTGRES_HOST")
 DB_NAME = os.environ.get("POSTGRES_DATABASE")
 DB_USER = os.environ.get("POSTGRES_USER")
 DB_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
-DB_PORT = "5432"  # 如果 .env.local 没有显式定义，可以手动指定
+DB_PORT = "5432" # default port
 
 conn = psycopg2.connect(
     host=DB_HOST,
@@ -21,12 +21,12 @@ conn = psycopg2.connect(
 def main():
     insert_query = """
     INSERT INTO properties 
-        (street, suburb, state, postcode, weekly_rent, photo, bedrooms, bathrooms, parking_spaces, property_type)
+        (street, suburb, state, postcode, weekly_rent, photo, bedrooms, bathrooms, parking_spaces, property_type, safety_score)
     VALUES 
-        (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
+        (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
     """
 
-    json_file_path = "property_data/parsed_properties_modified.json"
+    json_file_path = "parsed_properties_modified.json"
 
     with open(json_file_path, 'r', encoding='utf-8') as f:
         data = json.load(f)
@@ -43,7 +43,8 @@ def main():
             record.get("bedrooms"),
             record.get("bathrooms"),
             record.get("parking_spaces"),
-            record.get("property_type")
+            record.get("property_type"),
+            record.get("safety_score", 0.0)  # 如果 safety_score 没有，默认设为 0.0
         ))
     conn.commit()
     cur.close()
