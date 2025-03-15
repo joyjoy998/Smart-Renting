2ebe05ce3bdf3b2a04fcd137d8271a0e99e0ba94
Haochuan Zhou <mrzhouhc@gmail.com>
2025-03-16 10:50:05 +1100

    update map id

--- src/lib/constants/mapId.ts
+++ src/lib/constants/mapId.ts
@@ -1,2 +1,2 @@
-export const GOOGLE_LIGHT_MAPS_ID = "e6257723e90c8a60";
-export const GOOGLE_DARK_MAPS_ID = "9cc477a58fed0808";
+export const GOOGLE_LIGHT_MAPS_ID = "8500fe7cfdc8288b";
+export const GOOGLE_DARK_MAPS_ID = "28f07db8b8832aef";
