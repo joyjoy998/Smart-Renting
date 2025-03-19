7bc55dcddf82dbde55b66c5963ac7babec2da775
Claire Wu <wumeihui0207@gmail.com>
2025-03-19 23:41:05 +1100

    updated supabase recommendation function

--- src/stores/useSettingsStore.ts
+++ src/stores/useSettingsStore.ts
@@ -19,7 +19,7 @@ export const useSettingsStore = create<SettingsStore>((set) => ({
 
 export const useBudgetStore = create<BudgetStore>((set) => ({
   minPrice: 200,
-  maxPrice: 1000,
+  maxPrice: 600,
   setMinPrice: (price) => set((state) => ({ ...state, minPrice: price })),
   setMaxPrice: (price) => set((state) => ({ ...state, maxPrice: price })),
 }));
