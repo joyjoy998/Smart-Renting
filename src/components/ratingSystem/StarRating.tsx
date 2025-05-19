a482f2ff73c1c0c4510f453aea65f1981ad26353
yushu <zhuysh18@163.com>
2025-05-19 15:54:28 +1000

    the score is rounded to 2 decimal places

--- src/components/ratingSystem/StarRating.tsx
+++ src/components/ratingSystem/StarRating.tsx
@@ -1,4 +1,4 @@
-import PropTypes from "prop-type";
+import PropTypes from "prop-types";
 
 StarRating.propTypes = {
   maxRating: PropTypes.number,
@@ -29,20 +29,36 @@ export default function StarRating({
     color: color,
   };
 
-  const rating = Math.ceil(totalScore * maxRating);
+  const displayedStars = Math.min(maxRating, Math.ceil(totalScore * 10) * 0.5);
 
   return (
     <div style={containerStyle}>
       <div style={starContainerStyle}>
-        {Array.from({ length: maxRating }, (_, i) => (
-          <Star key={i} full={rating >= i + 1} color={color} />
-        ))}
+        {Array.from({ length: maxRating }, (_, i) => {
+          const currentIndex = i + 1;
+          return (
+            <Star
+              key={i}
+              full={currentIndex <= displayedStars}
+              half={currentIndex - 0.5 === displayedStars}
+              color={color}
+            />
+          );
+        })}
       </div>
     </div>
   );
 }
 
-function Star({ full, color }: { full: boolean; color: string }) {
+function Star({
+  full,
+  half,
+  color,
+}: {
+  full: boolean;
+  half: boolean;
+  color: string;
+}) {
   const starStyle = {
     width: "18px",
     height: "18px",
@@ -59,6 +75,24 @@ function Star({ full, color }: { full: boolean; color: string }) {
           stroke={color}
         >
           <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+        </svg>
+      ) : half ? (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill={color}
+          stroke={color}
+        >
+          <defs>
+            <linearGradient id="halfGradient">
+              <stop offset="50%" stopColor={color} />
+              <stop offset="50%" stopColor="transparent" />
+            </linearGradient>
+          </defs>
+          <path
+            fill="url(#halfGradient)"
+            d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+          />
         </svg>
       ) : (
         <svg
